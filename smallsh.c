97f62464907a52949c88d73dba54dd6cb0484e71
Cody DePeel <codydepeel@gmail.com>
2023-11-13 22:41:24 -0800

    added functionality for status()

--- smallsh.c
+++ smallsh.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 
 #define MAX_LENGTH 2048  // The maximum length of a command
 #define MAX_ARGS 512     // The maximum number of arguments
 
-// HINT 1: Consider defining a struct to store all the different elements included in a command
+int exitMethod = -5;  // The status of the last foreground process
+
 struct sigaction SIGINT_action = {0};
 struct sigaction SIGTSTP_action = {0};
 
 // Function that handles the expansion of $$ in the input string
 void handle_expansion(){
-
+   
 }
 
 
 void signal_handlers(){
+   
    // Handle CTRL+C
 
 
@@ -55,12 +59,41 @@ void exit_program(){
    exit(0);
 }
 
-void change_directory(){
-   printf("In change_directory...\n");
+void change_directory(char** arg_list, int num_of_args){
+   int status;
+
+   if(num_of_args == 1){
+      // If no arguments, change to the HOME directory
+      status = chdir(getenv("HOME"));
+   }
+   else{
+      // A file path was provided -- in SECOND argument (1st is "cd")
+      status = chdir(arg_list[1]);
+   }
+
+   // If the user tries to go to a bad directory, tell the user
+   if(status == -1){  // chdir() returns -1 if error occurred
+      printf("ERROR: Directory not found.\n");
+      fflush(stdout);
+   }
 }
 
 void status(){
-   printf("In status...\n");
+   // printf("In status...\n");
+
+   // Return "exit status 0" if no foreground process has been run yet
+   if(exitMethod == -5)
+      printf("exit status 0\n");
+   
+   // If child process terminates normally, print exit status
+   else if(WIFEXITED(exitMethod) != 0)
+      printf("exit status %d\n", WEXITSTATUS(exitMethod));
+   
+   // If child process was terminated by a signal, print signal number
+   else
+      printf("terminated by signal %d\n", WTERMSIG(exitMethod));
+   fflush(stdout);
+
 }
 
 void parse_cmd(char** arg_list, int num_of_args){
@@ -77,13 +110,13 @@ void parse_cmd(char** arg_list, int num_of_args){
 
    // If the command is "exit", exit the shell
    if(strcmp(arg_list[0], "exit") == 0){
-      exit_program();
+      exit_program(arg_list, num_of_args);
    }
    else if(strcmp(arg_list[0], "cd") == 0){
-      change_directory();
+      change_directory(arg_list, num_of_args);
    }
    else if(strcmp(arg_list[0], "status") == 0){
-      status();
+      status(arg_list, num_of_args);
    }
    else{  // For all non-built-in commands, parent will fork off a child
       
@@ -104,7 +137,7 @@ int main(){
       int num_of_args = user_cmd(arg_list, input_buffer);  // User inputs for shell cmds
       arg_list[num_of_args] = NULL;  // Set the last element to NULL
       
-      printf("In main...\n");
+      // printf("In main...\n");
 
       parse_cmd(arg_list, num_of_args);  // Functionality of each of the cmds
    }
